a22b9fd3a797f85a7a3d93adcfc02917984ade38
Var Bhat <mailvarbhat@gmail.com>
2022-02-23 23:15:21 +0530

    corrected

--- dpagelink.go
+++ dpagelink.go
@@ -61,10 +61,16 @@ func getDpageLink(aid string, epno string) string {
 
 func decryptDLink(iurl string) []Link {
 	ajax_url := "https://gogoplay.io/encrypt-ajax.php"
-	//secret_key := "3235373436353338353932393338333936373634363632383739383333323838"
-	// iv := "34323036393133333738303038313335"
-	//rtime := "69420691337800813569"
-	rtime := "00000000000000000000"
+	var rtime string
+	var secret_key string
+	var ajax string
+
+	// Get video id
+	iUrl, err := url.Parse(iurl)
+	if err != nil {
+		return []Link{}
+	}
+	video_id := iUrl.Query().Get("id")
 
 	response, err := client.Get(iurl).End()
 	if err != nil {
@@ -77,15 +83,16 @@ func decryptDLink(iurl string) []Link {
 
 	encrypted := doc.Find("script[data-name='crypto']").AttrOr("data-value", "")
 	iv := doc.Find("script[data-name='ts']").AttrOr("data-value", "")
-	secret_key := aes256decrypt(encrypted, []byte(iv+iv), []byte(iv), aes.BlockSize)
-
-	iUrl, err := url.Parse(iurl)
-	if err != nil {
-		return []Link{}
+	if encrypted == "" || iv == "" {
+		secret_key = "3235373436353338353932393338333936373634363632383739383333323838"
+		iv = "34323036393133333738303038313335"
+		rtime = "69420691337800813569"
+		ajax = AES256Encrypt(secret_key, iv, video_id)
+	} else {
+		rtime = "00000000000000000000"
+		secret_key = aes256decrypt(encrypted, []byte(iv+iv), []byte(iv), aes.BlockSize)
+		ajax = aes256encrypt([]byte(video_id), []byte(secret_key), []byte("0000000000000000"), aes.BlockSize)
 	}
-	video_id := iUrl.Query().Get("id")
-
-	ajax := aes256encrypt([]byte(video_id), []byte(secret_key), []byte("0000000000000000"), aes.BlockSize)
 
 	var rbody = []byte(fmt.Sprintf("id=%s&time=%s", ajax, rtime))
 	req, _ := http.NewRequest("POST", ajax_url, bytes.NewBuffer(rbody))
