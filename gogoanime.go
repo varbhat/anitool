2713c4bf464c22fa03cb633016c76926d56f68d9
Var Bhat <mailvarbhat@gmail.com>
2022-04-16 21:26:40 +0530

    improved

--- gogoanime.go
+++ gogoanime.go
@@ -42,7 +42,13 @@ func (g *GogoAnime) FromMAL(iurl string) (err error) {
 	return nil
 }
 
-func (g *GogoAnime) GetLinks() (Ret chan Link) {
+func (g *GogoAnime) GetLinks(subordub string, epno string) (Ret chan Link) {
+	var aid string
+	if subordub == "dub" {
+		aid = g.Dub
+	} else {
+		aid = g.Sub
+	}
 	var gogobaseurl string = "https://gogoanime.fi"
 	gogobaseurl, err := finalURL(gogobaseurl)
 	if err != nil {
